282d6792806b4e051f9b3e374023e9b49e463f02
Syed Ahkam <smahkam57@gmail.com>
2024-04-05 06:49:29 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/glyph_studio/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.glyph_studio
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ devshell.nix
@@ -0,0 +1,28 @@
+{ pkgs }:
+
+with pkgs;
+
+# Configure your development environment.
+#
+# Documentation: https://github.com/numtide/devshell
+devshell.mkShell {
+  name = "glyph-studio";
+  motd = ''
+    Ready to hack ‍🔥
+  '';
+  env = [
+    {
+      name = "ANDROID_HOME";
+      value = "${android-sdk}/share/android-sdk";
+    }
+    {
+      name = "ANDROID_SDK_ROOT";
+      value = "${android-sdk}/share/android-sdk";
+    }
+    {
+      name = "JAVA_HOME";
+      value = jdk.home;
+    }
+  ];
+  packages = [ android-sdk gradle jdk flutter ];
+}
