3a3328c5277cb01256e46a524ccbb745d3d9bbb9
Syed Ahkam <smahkam57@gmail.com>
2024-04-06 05:49:46 +0530

    fix: dev shell; somewhat

--- flake.nix
+++ flake.nix
@@ -1,73 +1,100 @@
 {
-  description = "Dev configuration for a flutter based app with android-sdk";
-
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs";
-    devshell.url = "github:numtide/devshell";
-    flake-utils.url = "github:numtide/flake-utils";
-    android.url = "github:tadfisher/android-nixpkgs";
-    flutter-nix.url = "github:maximoffua/flutter.nix/stable";
-    nixgl.url = "github:nix-community/nixGL";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    systems.url = "github:nix-systems/default";
+    devenv.url = "github:cachix/devenv";
+
+    android-nixpkgs = {
+      url = "github:tadfisher/android-nixpkgs";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    flutter-nix = {
+      url = "github:maximoffua/flutter.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs =
-    { self, nixpkgs, devshell, flake-utils, android, flutter-nix, nixgl }:
-    {
-      overlay = final: prev: {
-        inherit (self.packages.${final.system}) android-sdk android-studio;
-      };
-    } // flake-utils.lib.eachSystem [
-      "aarch64-darwin"
-      "x86_64-darwin"
-      "x86_64-linux"
-    ] (system:
-      let
-        inherit (nixpkgs) lib;
-        pkgs = import nixpkgs {
-          inherit system;
-          config.allowUnfree = true;
-          overlays = [
-            devshell.overlays.default
-            flutter-nix.overlays.default
-            nixgl.overlay
-            self.overlay
-          ];
-        };
-      in {
-        packages = {
-          nixgl = nixgl.nixGlDefault;
-          flutter = flutter-nix.packages.${system}.flutter;
-          android-sdk = android.sdk.${system} (sdkPkgs:
-            with sdkPkgs;
-            [
-              # Useful packages for building and testing.
-              build-tools-30-0-3
-              cmdline-tools-latest
-              emulator
-              platform-tools
-              platforms-android-33
-              system-images-android-33-google-apis-x86-64
+  nixConfig = {
+    extra-trusted-public-keys =
+      "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=";
+    extra-substituters = "https://devenv.cachix.org";
+  };
 
-              # Other useful packages for a development environment.
-              # ndk-26-1-10909125
-              # skiaparser-3
-              # sources-android-33
-            ] ++ lib.optionals (system == "aarch64-darwin") [
-              # system-images-android-33-google-apis-arm64-v8a
-              # system-images-android-33-google-apis-playstore-arm64-v8a
-            ] ++ lib.optionals
-            (system == "x86_64-darwin" || system == "x86_64-linux") [
-              # system-images-android-33-google-apis-x86-64
-              # system-images-android-33-google-apis-playstore-x86-64
-            ]);
-        } // lib.optionalAttrs (system == "x86_64-linux") {
-          # Android Studio in nixpkgs is currently packaged for x86_64-linux only.
-          # android-studio = pkgs.androidStudioPackages.stable;
-          # android-studio = pkgs.androidStudioPackages.beta;
-          # android-studio = pkgs.androidStudioPackages.preview;
-          # android-studio = pkgs.androidStudioPackage.canary;
-        };
-
-        devShell = import ./devshell.nix { inherit pkgs; };
+  outputs = { self, nixpkgs, devenv, systems, ... }@inputs:
+    let forEachSystem = nixpkgs.lib.genAttrs (import systems);
+    in {
+      packages = forEachSystem (system: {
+        devenv-up = self.devShells.${system}.default.config.procfileScript;
       });
+
+      devShells = forEachSystem (system:
+        let pkgs = nixpkgs.legacyPackages.${system};
+        in {
+          default = let
+            inherit (inputs) flutter-nix android-nixpkgs;
+            flutter-sdk = flutter-nix.packages.${system};
+            sdk = (import android-nixpkgs { }).sdk (sdkPkgs:
+              with sdkPkgs; [
+                build-tools-30-0-3
+                build-tools-34-0-0
+                cmdline-tools-latest
+                emulator
+                platform-tools
+                platforms-android-34
+                platforms-android-33
+                platforms-android-31
+                platforms-android-28
+                system-images-android-34-google-apis-playstore-x86-64
+              ]);
+          in devenv.lib.mkShell {
+            inherit inputs pkgs;
+            modules = [
+              ({ pkgs, config, ... }: {
+                # https://devenv.sh/basics/
+                # dotenv.enable = true;
+                env.ANDROID_AVD_HOME = "${config.env.DEVENV_ROOT}/.android/avd";
+                env.ANDROID_SDK_ROOT = "${sdk}/share/android-sdk";
+                env.ANDROID_HOME = config.env.ANDROID_SDK_ROOT;
+                env.CHROME_EXECUTABLE = "chromium";
+                env.FLUTTER_SDK = "${pkgs.flutter}";
+                env.GRADLE_OPTS =
+                  "-Dorg.gradle.project.android.aapt2FromMavenOverride=${sdk}/share/android-sdk/build-tools/34.0.0/aapt2";
+
+                # https://devenv.sh/packages/
+                packages = [ flutter-sdk.flutter ];
+
+                # https://devenv.sh/scripts/
+                # Create the initial AVD that's needed by the emulator
+                scripts.create-avd.exec =
+                  "avdmanager create avd --force --name phone --package 'system-images;android-34;google_apis_playstore;x86_64'";
+
+                # https://devenv.sh/processes/
+                # These processes will all run whenever we run `devenv run`
+                processes.emulator.exec = "nixGLMesa emulator -avd phone -skin 720x1280";
+                processes.generate.exec = "dart run build_runner watch || true";
+                # processes.grovero-app.exec = "flutter run lib/main.dart";
+
+                enterShell = ''
+                  mkdir -p $ANDROID_AVD_HOME
+                  export PATH="${sdk}/bin:$PATH"
+                  export FLUTTER_GRADLE_PLUGIN_BUILDDIR="''${XDG_CACHE_HOME:-$HOME/.cache}/flutter/gradle-plugin";
+                '';
+
+                # https://devenv.sh/languages/
+                languages.dart = {
+                  enable = true;
+                  package = flutter-sdk.dart;
+                };
+                languages.java = {
+                  enable = true;
+                  gradle.enable = false;
+                  jdk.package = pkgs.jdk17;
+                };
+
+                # See full reference at https://devenv.sh/reference/options/
+              })
+            ];
+          };
+        });
+    };
 }
